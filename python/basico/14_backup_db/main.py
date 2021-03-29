402c8a874701fb61bbcb8fcf49ebaff6062c51cc
Christian_git_hub <christianipc.1987@gmail.com>
2021-03-29 00:21:39 -0300

    python/basico, 14_backup_db: agrego la opcion de setear el usuario y agrego mensajes de ayuda

--- python/basico/14_backup_db/main.py
+++ python/basico/14_backup_db/main.py
@@ -19,25 +19,60 @@
 import os, sys, subprocess, shlex
 
 
+def msj_help():
+    print('\n')
+    print('Para más ayuda ejecute python main.py help')
+    print('\n')
+
+def resultado_verificacion(resultado,msj):
+    if not resultado:
+        print('Dump Realizado')
+    else:
+        print('\n')
+        print(msj)
+        msj_help()
+
 def main():
     try:
         # obtengo los parametros ingresados si es que hay alguno
         parametros = sys.argv[-1] if len(sys.argv) >= 3 else None
 
-        ## en caso de no haber ninguno
-        if parametros is None:
+        # en caso de no haber ningun parámetro más que el nombre de la base de datos 
+        # y si no esta ingresando el parametro help
+        if parametros is None and 'help' not in sys.argv:
             dump = f'pg_dump -U postgres -d {sys.argv[1]} -f dump_{sys.argv[1]}.sql'
             resultado = subprocess.call(shlex.split(dump))
-            msj = 'Dump realizado' if resultado == 0 else 'Error. La base da datos ingresada no existe o tal vez el usuario postgres no tenga permisos necesarios para realizar el dump, pruebe ingresando un usuario despues del parametro python main.py -U [USUARIO] -d [DB_NAME]'
-            print(msj)
-        ## en caso de tener el parametro que asigna base de datos
+            # msj = 'Dump realizado' if resultado == 0 else 'Error. La base da datos ingresada no existe o tal vez el usuario postgres no tenga permisos necesarios para realizar el dump, pruebe ingresando un usuario despues del parametro python main.py -U [USUARIO] -d [DB_NAME]'
+            resultado_verificacion(resultado,'Error. La base da datos ingresada no existe o tal vez el usuario postgres no tenga permisos necesarios para realizar el dump, pruebe ingresando un usuario despues del parametro python main.py -u [USUARIO] -d [DB_NAME]')
+
+        ## en caso de tener el parametro para asignar usuario 
         elif '-u' in sys.argv:
-            print('Ingreso el usuario')
+            # obtengo el parámetro de usuario y el posterior del array
+            indice_usuario = sys.argv.index('-u')
+            usuario = sys.argv[indice_usuario + 1]
+           
+            # obtengo el parámetro de la base de datos y el posterior del array
+            indice_db = sys.argv.index('-d') if '-d' in sys.argv else None
+            db = sys.argv[indice_db + 1]
+           
+            dump = f'pg_dump -U {usuario} -d {db} -f dump_{db}.sql'
+            resultado = subprocess.call(shlex.split(dump))
+            # msj = 'Dump realizado' if resultado == 0 else 'Error. Usuario incorrecto o la base da datos ingresada no existe.'
+            resultado_verificacion(resultado,'Error. Usuario incorrecto o la base da datos ingresada no existe.')
+
+        elif 'help' in sys.argv:
+            print('\n\t *************** Ayudas ******************\n')
+            print('dump de una base de datos suponiendo que el usuario es postgres y estoy en el mismo servidor de la db:')
+            print('\tpython main.py [nombre_bd]\n')   
+            print('dump de una base de datos ingresando el nombre del usuario y nombre de la base de datos:')
+            print('\tpython main.py -u [nombre_usuario] -d [nombre_bd]')  
+            print('\n')
         else:
             print('hay parametros')
 
     except Exception as e:
         print(f'Error: {e} ')
+        msj_help()
 
 
 if __name__ == "__main__":
